e78746ce1cedba191ece828d004a57ed7c0a08c7
fais edathil <faiesedathil@gmail.com>
2022-08-23 21:29:34 +0300

    Updates

---
+++ precache-manifest.935d32c9e18432c0f40cc05327b4b4b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "258ed832c196c20b7ea4",
+    "url": "/netflix-clone-react-app/static/css/main.8332a050.chunk.css"
+  },
+  {
+    "revision": "258ed832c196c20b7ea4",
+    "url": "/netflix-clone-react-app/static/js/main.258ed832.chunk.js"
+  },
+  {
+    "revision": "42d700ca32814a378051",
+    "url": "/netflix-clone-react-app/static/js/1.42d700ca.chunk.js"
+  },
+  {
+    "revision": "aac008b865a3a31cd2e0",
+    "url": "/netflix-clone-react-app/static/js/runtime~main.aac008b8.js"
+  },
+  {
+    "revision": "698b4cd9b848422ca74ae07888ace12b",
+    "url": "/netflix-clone-react-app/index.html"
+  }
+];
